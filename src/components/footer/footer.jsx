7fb599f00091f0973a28031696b8c08f8fcd4586
GannonDetroit <42745542+GannonDetroit@users.noreply.github.com>
2020-03-07 02:23:47 -0500

    made it more responsive and mobile view friendly

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -4,6 +4,8 @@ import './footer.styles.scss'
 const Footer = () => (
     <div className='footer'>
         <p>P.S: We Need To Take More Photos Together...</p>
+        <p>Developer Notes: These photos are not edited to be similar size so making this a responsive frontend site for mobile, tablet, and desktop got a bit tricky since the views are all the same size. I'll work on my photo editing skills for next year lol.
+        </p>
     </div>
 )
 
