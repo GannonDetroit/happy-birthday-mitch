94e8c5176ed8ed54346435b2de0f6cfe720344c7
GannonDetroit <42745542+GannonDetroit@users.noreply.github.com>
2020-03-07 00:10:35 -0500

    added a basic starting header, google font, and footer component architecture I forgot earlier

---
+++ src/components/header/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './header.styles.scss'
+
+const Header = () => (
+    <div className='headerWrapper'>
+        <h1 className='title'> HAPPY BIRTHDAY MITCH</h1>
+        <span>Love your best friend - Gannon</span>
+    </div>
+)
+
+export default Header
