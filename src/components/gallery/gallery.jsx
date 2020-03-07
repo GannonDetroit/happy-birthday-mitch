7a1c51d78a64fcf84d0f8ee11be2e7af5159b94f
GannonDetroit <42745542+GannonDetroit@users.noreply.github.com>
2020-03-07 02:38:55 -0500

    last minute style change and updated the description state

--- src/components/gallery/gallery.jsx
+++ src/components/gallery/gallery.jsx
@@ -26,62 +26,62 @@ class Gallery extends React.Component{
             {
                 id: 1,
                 image: photo1,
-                description: 'test description'
+                description: 'Meeting your family and hanging out downtown for your wedding was a blast.'
             },
             {
                 id: 2,
                 image: photo2,
-                description: 'test description'
+                description: 'I was (am) so proud of your wedding and marriage to Hayley. You guys did it right and I was honored and thrilled to be a part of something so beautiful.'
             },
             {
                 id: 3,
                 image: photo3,
-                description: 'test description'
+                description: 'Your bachelor party was perfect. We all had fun, it was the perfect venue, and its memories that I’ll enjoy for the rest of my life.'
             },
             {
                 id: 4,
                 image: photo4,
-                description: 'test description'
+                description: 'I’ll forever be thankful to EMU for pairing us up to be roommates for college. I’ll also be forever thankful for your dumb ass never leaving :) even when I annoyed the shit out of you.'
             },
             {
                 id: 5,
                 image: photo5,
-                description: 'test description'
+                description: 'It\'s fun telling people we\'re brothers and they believe it because we look we are and be God you are a brother to me.'
             },
             {
                 id: 6,
                 image: photo6,
-                description: 'test description'
+                description: 'I loved going on adventures with you and our friends. (I think this was the color run we did) we need to do more cool shit again soon.'
             },
             {
                 id: 7,
                 image: photo7,
-                description: 'test description'
+                description: 'yep, most certainly the color run but it looks like we were kicking it at a badass rave... at least thats what I\'ll tell the kids'
             },
             {
                 id: 8,
                 image: photo8,
-                description: 'test description'
+                description: 'I do miss our epic nerf gun fights we use to have at the old apartment... even though your shotgun was OP as fuck'
             },
             {
                 id: 9,
                 image: photo9,
-                description: 'test description'
+                description: 'I also miss your amazing cooking.... and stealing credit for it whenever I brought a girl over to the apartment lol'
             },
             {
                 id: 10,
                 image: photo10,
-                description: 'test description'
+                description: 'WE NEED TO DO OUTDOORS SHIT SOON! I miss kayaking and adventuring (and bitching)'
             },
             {
                 id: 11,
                 image: photo11,
-                description: 'test description'
+                description: 'We will also need to plan for more international travel... around the end of March 2023 would be good for me lol'
             },
             {
                 id: 12,
                 image: photo12,
-                description: 'test description'
+                description: 'I legit don\'t remember what this was about... but I found it on my phone and love it'
             },
         ]
 
